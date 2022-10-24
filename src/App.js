4acd99c6e613fad5e28b28f1e4b758f966975659
João Coqueiro <jaaum.p95@gmail.com>
2022-10-24 16:10:59 -0300

    FEAT: Requisitos 1 a 6

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React from 'react';
-import './App.css';
-import rockGlass from './images/rockGlass.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import Meals from './pages/Meals';
 
-function App() {
+export default function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <Switch>
+      <Route exact path="/" component={ Login } />
+      <Route exact path="/meals" component={ Meals } />
+    </Switch>
   );
 }
-
-export default App;
